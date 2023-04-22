ae8f5bab2be048b343d8f0eec21fef3e2332c0f5
Eva-Lynn <evaosas1@gmail.com>
2023-04-21 20:37:52 -0700

    last

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: to print char in lower case only
+ * Description: to print char in lower and upper case
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n = 97;
+        int n = 97;
 	int m = 65;
-	
-	while (n < 123)
-	{
-		putchar(n);
-		n++;
-	}
+
+        while (n < 123)
+        {
+                putchar(n);
+                n++;
+        }
 	while (m <= 90)
 	{
 		putchar(m);
 		m++;
 	}
-	putchar('\n');
-	return (0);
+        putchar('\n');
+        return (0);
 }
-
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: to print in Hex
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+	int n;
+
+	for (m = 48; m <= 57; m++)
+	{
+		putchar(m);
+	}
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
