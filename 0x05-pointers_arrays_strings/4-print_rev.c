63fb25e17bd006ba4b30e869480e04e7cf79176f
Eva-Lynn <evaosas1@gmail.com>
2023-05-02 18:52:32 -0700

    2

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse
+ * @*s: string to check
+ */
+void print_rev(char *s)
+{
+	int a, b;
+	b = 0;
+
+	while (s[b] != '\0')
+		b++;
+	for (a = b -1; a >=  0; a--;)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
